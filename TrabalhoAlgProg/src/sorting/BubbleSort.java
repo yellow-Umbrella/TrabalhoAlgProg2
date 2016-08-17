10f1964b46969a92e75bf45aa6679ac6b8a2d4b4
Victor <Victor@10.6.94.142>
2016-08-17 19:43:56 -0400

    Bubble sort

--- TrabalhoAlgProg/src/sorting/BubbleSort.java
+++ TrabalhoAlgProg/src/sorting/BubbleSort.java
@@ -1,5 +1,34 @@
 package sorting;
+import sorting.DadSort;
 
-public class BubbleSort {
+/* 	 Ordenação “por bolha”
+   	 É considerado um dos algoritmos mais simples para ordenação
+   	 Em geral, se pensarmos em ordenar uma sequência é uma das primeiras ideias que temos
+   	 A ideia é percorrer o vetor diversas vezes, a cada passagem fazendo o maior elemento “flutuar" para o final da sequência
+   	 MUITO RUIM! 
+   
+   	 Estável
+   
+   	 Tempo:
+   		 Pior caso: O(n2)
+   		 Caso médio: O(n2)
+		 Melhor caso: O(n) [Já otimizado para verificar se não ocorreu nenhuma troca]
+		
+	 Espaço:
+		 Pior caso: O(n) total, O(1) auxiliar */
+
+public class BubbleSort extends DadSort {
+
+	@Override
+	public void sort(int[] vet) {
+		
+		for (int i = 0; i < vet.length; i++) {
+			for (int j = 0 ; j < vet.length; j++) {
+				if (vet[j] > vet[j+1])
+					swap(vet, j, (j + 1));
+			}
+		}
+
+	}
 
 }
---
+++ TrabalhoAlgProg/src/sorting/doSort.java
@@ -0,0 +1,7 @@
+package sorting;
+
+public interface doSort {
+	void sort(int[] vet);
+	void swap(int[] vet, int i, int j);
+	void print(int[] vet);
+}
