0319667d40a981c5d633ee8cb74e888a57e14f85
Victor <Victor@10.6.185.57>
2016-08-19 14:34:33 -0400

    Teste Main

--- TrabalhoAlgProg/src/main/Main.java
+++ TrabalhoAlgProg/src/main/Main.java
@@ -1,8 +1,18 @@
 package main;
 
+import java.io.IOException;
+
+import linkedSort.BubbleLinked;
+import util.FileRandom;
+import util.LinkedList;
+
 public class Main {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		FileRandom file = new FileRandom();
+		LinkedList list = file.reader("1000.txt");
+		BubbleLinked bubble = new BubbleLinked();
+		bubble.sort(list);
+		System.out.println(list);
 	}
 
 }
