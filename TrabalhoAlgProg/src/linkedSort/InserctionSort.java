40c99be049842911194ab9485a8c914c0a8b600f
Victor <Victor@10.6.185.57>
2016-08-19 14:18:39 -0400

    Inserction, Merge, Quick
    #IMPORTANTE: mudar sistema que verifica por index, pois será necessário
    percorrer muitas vezes cada lista para encontrar os index.
    
    #IMPORTANTE: validar pivot do QuickSort

--- TrabalhoAlgProg/src/linkedSort/InserctionSort.java
+++ TrabalhoAlgProg/src/linkedSort/InserctionSort.java
@@ -1,5 +1,19 @@
 package linkedSort;
 
-public class InserctionSort {
+import util.LinkedList;
 
+public class InserctionSort {
+	
+	public void sort (LinkedList list) {
+		int j;
+		
+		for (int i = 0; i < list.size(); i++) {
+			j = i;
+			while (j > 0 && list.getValue(j-1) > list.getValue(j)) {
+				list.swap(j, j - 1);
+				j--;
+			}
+		}
+		
+	}
 }
--- TrabalhoAlgProg/src/linkedSort/QuickSort.java
+++ TrabalhoAlgProg/src/linkedSort/QuickSort.java
@@ -1,5 +1,37 @@
 package linkedSort;
 
-public class QuickSort {
+import util.LinkedList;
 
+public class QuickSort {
+	
+	public void sort (LinkedList list, int init, int end) {
+		
+		if (init < end) {
+			int pivot = partition(list, init, end);
+			sort(list, init, (pivot - 1));
+			sort(list, pivot, end);
+		}
+		
+	}
+
+	private int partition(LinkedList list, int init, int end) {
+		int i = init;
+		int j = end;
+		
+		int pivot = list.getValue(init + (end - init));
+		
+		while (i <= j) {
+			while (list.getValue(i) < pivot)
+				i++;
+			while (list.getValue(j) > pivot)
+				j--;
+		}
+		return i;
+	}
+
+	private int getPivot(LinkedList list, int init, int end) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
 }
