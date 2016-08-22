1a765c5893d35a04656834532bb7b6e22c1c871c
Victor <Victor@10.6.100.218>
2016-08-22 16:18:16 -0400

    Remoção da Main antiga

--- TrabalhoAlgProg/src/linkedSort/BubbleSort.java
+++ TrabalhoAlgProg/src/linkedSort/BubbleSort.java
@@ -1,15 +1,18 @@
 package linkedSort;
 
+import java.util.Iterator;
+
 import util.LinkedList;
 
 public class BubbleSort {
 	
 	public void sort(LinkedList list) {
 		
-		for(int i = 0; i < list.size()-1; i++) {	//for(Iterator<Integer> it = list.iterator(); it.hasNext();)
-			for(int j = 0; j < list.size()-1; j++) {
-				if(list.getValue(j) > list.getValue(j+1))
-					list.swap(j, j+1);
+		for(int i = 0; i < list.size()-1; i++) {
+			for(Iterator<Integer> j = list.iterator(); j.hasNext();) {
+				int value1 = j.next(), value2 = j.next();
+				if(value1 > value2)
+					list.swap(value1, value2);
 			}
 		}
 	}
