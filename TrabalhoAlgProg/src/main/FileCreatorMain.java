804ecb128495cd989858238486b78e41501078c1
Victor <Victor@10.6.246.209>
2016-08-23 17:50:55 -0400

    Alteração no método contains (FileRandom), utilização de um vetor de boolean para comparação;

--- TrabalhoAlgProg/src/main/FileCreatorMain.java
+++ TrabalhoAlgProg/src/main/FileCreatorMain.java
@@ -12,17 +12,17 @@ public class FileCreatorMain {
 		// Cria os arquivos de (nome.txt, número de inteiros)
 		file.writer("10.txt", 10);
 		
-//		file.writer("1000.txt", 1000);
-//		
-//		file.writer("5000.txt", 5000);
-//		
-//		file.writer("10000.txt", 10000);
-//		
-//		file.writer("20000.txt", 20000);
-//		
-//		file.writer("50000.txt", 50000);
-//		
-//		file.writer("100000.txt", 100000);
+		file.writer("1000.txt", 1000);
+		
+		file.writer("5000.txt", 5000);
+		
+		file.writer("10000.txt", 10000);
+		
+		file.writer("20000.txt", 20000);
+		
+		file.writer("50000.txt", 50000);
+		
+		file.writer("100000.txt", 100000);
 //		
 	}
 
--- TrabalhoAlgProg/src/util/FileRandom.java
+++ TrabalhoAlgProg/src/util/FileRandom.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.util.Random;
 
 public class FileRandom {
-	private int posicao = 0; // variável útil para criação de arquivo sem repetição
 	
 	// Lê um arquivo e adiciona os elementos à lista
 	public LinkedList reader(String path) throws IOException { // recebe (endereço.txt)
@@ -25,11 +24,11 @@ public class FileRandom {
 	// Escreve em um arquivo inteiros não repetidos
 	public void writer(String path, int length) throws IOException { // recebe (endeço.txt, número de inteiros)
 		BufferedWriter out = new BufferedWriter(new FileWriter(path));
-		int[] vet = new int[length]; // vetor para verificação de repetições
-		this.posicao = 0; // inicia a escrita do vetor na posicao 0
+		length *= 10;
+		boolean[] vet = new boolean[length]; // vetor para verificação de repetições
 		
 		for (int i = 0; i < length; i++) {
-			out.write(notEqual(vet)); // adicina no arquivo o valor criado pelo método notEqual
+			out.write(contains(vet, length)); // adicina no arquivo o valor criado pelo método notEqual
 			if (i != length-1)
 				out.newLine(); // adiciona quebra de linha no arquivo
 			out.flush();
@@ -39,16 +38,13 @@ public class FileRandom {
 	}
 	
 	// Valida o valor antes de ser escrito no arquivo
-	private String notEqual(int[] vet) throws IOException { // recebe o vetor com os demais valores do arquivo
+	private String contains(boolean[] vet, int length) throws IOException { // recebe o vetor com os demais valores do arquivo
 		Random rand = new Random();
-		int aux = rand.nextInt(10);//Integer.MAX_VALUE); // variável recebe um valor randômico inteiro de 0 a MAX_VALUE
-		
-		for (int i = 0; i < posicao; i++) { // percorre o vetor
-			if(vet[i] == aux) // se valor já existe no vetor, chama notEqual novamente
-				return notEqual(vet);
-		}
-		vet[posicao] = aux; // adiciona variável no vetor[posicao]
-		posicao++; // incrementa posicao
+		int aux = rand.nextInt(length-1); // variável recebe um valor randômico inteiro de 0 a length
+		if (vet[aux] == false)
+			vet[aux] = true; // adiciona variável
+		else
+			return contains(vet, length); // tenta outro número
 		return aux + ""; // retorna variável no farmato de String
 	}
 }
