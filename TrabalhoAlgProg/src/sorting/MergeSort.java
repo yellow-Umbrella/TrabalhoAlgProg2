5d92c59fbebce3ec2c0dcb881be2a25105f1530f
Victor <Victor@Victor>
2016-08-24 00:06:02 -0400

    Esqueleto Merge e Quick

---
+++ TrabalhoAlgProg/src/sorting/MergeSort.java
@@ -0,0 +1,16 @@
+package sorting;
+
+import util.LinkedList;
+import util.Node;
+
+public class MergeSort {
+	
+	private void merge(LinkedList list, Node start, Node middle, Node end) {
+		
+	}
+	
+	public void sort(LinkedList list, Node start, Node end) {
+		
+	}
+	
+}
---
+++ TrabalhoAlgProg/src/sorting/QuickSort.java
@@ -0,0 +1,16 @@
+package sorting;
+
+import util.LinkedList;
+import util.Node;
+
+public class QuickSort {
+
+	private void partition(LinkedList list, Node init, Node end) {
+		
+	}
+	
+	public void sort(LinkedList list, Node init, Node end) {
+		
+	}
+
+}
