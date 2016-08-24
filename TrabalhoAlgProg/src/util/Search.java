8e001ce28681946f410709aa1d72136e5e6fd4b5
Victor <Victor@10.6.84.217>
2016-08-24 19:26:21 -0400

    Search - Sequential - OK 		 Binary - ERRO

--- TrabalhoAlgProg/src/util/Search.java
+++ TrabalhoAlgProg/src/util/Search.java
@@ -14,24 +14,21 @@ public class Search {
 	}
 	
 	public boolean binary(LinkedList list, int value) {
-		return binaria(list, value, 0, list.size()-1);
+		return binaria(list, value, 0, list.size() - 1);
 	}
 	
 	private boolean binaria(LinkedList list, int value, int start, int end) {
-		int middle = (end-start)/2;
-		Node no = list.get(middle);
-		
-		if(no.getValue() == value)
-			return true;
-		else if (start >= end)
+		if(start > end)
 			return false;
-		else if(no.getValue() > value) {
-			return binaria(list, value, start, middle - 1);
+		else {
+			int middle = (end - start)/2, compare = list.get(middle).getValue();
+			if(compare == value)
+				return true;
+			else if(value > compare)
+				return binaria(list, value, middle + 1, end);
+			else
+				return binaria(list, value, start, middle - 1);
 		}
-		else if (no.getValue() < value)
-			return binaria(list, value, middle + 1, end);
-		else
-			return false;
 	}
 	
 }
