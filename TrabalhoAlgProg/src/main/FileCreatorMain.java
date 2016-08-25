2122daf342db75e8bdb916d85018241ae72f50e6
Victor <Victor@10.6.75.47>
2016-08-25 18:27:20 -0400

    Reducao de classes;

--- TrabalhoAlgProg/src/main/FileCreatorMain.java
+++ TrabalhoAlgProg/src/main/FileCreatorMain.java
@@ -17,19 +17,21 @@ public class FileCreatorMain {
 			// Cria os arquivos de (nome.txt, número de inteiros)
 			
 			file.writer("10.txt", 10);
-			
 			file.writer("1000.txt", 1000);
-			
 			file.writer("5000.txt", 5000);
-			
 			file.writer("10000.txt", 10000);
-			
 			file.writer("20000.txt", 20000);
-			
 			file.writer("50000.txt", 50000);
-			
 			file.writer("100000.txt", 100000);
+			
+			file.writer100("100 - 1000.txt", "1000.txt");
+			file.writer100("100 - 5000.txt", "5000.txt");
+			file.writer100("100 - 10000.txt", "10000.txt");
+			file.writer100("100 - 20000.txt", "20000.txt");
+			file.writer100("100 - 50000.txt", "50000.txt");
+			file.writer100("100 - 100000.txt", "100000.txt");
 		}
+		
 		
 		scan.close();
 	}
