2ee6ec441843d841273953fa19fba102044b358c
Victor <Victor@Victor>
2016-08-28 16:45:30 -0400

    teste

--- TrabalhoAlgProg/src/toDo/QuickSort.java
+++ TrabalhoAlgProg/src/toDo/QuickSort.java
@@ -5,14 +5,24 @@ import util.Node;
 
 public class QuickSort {
 
-	private void partition(LinkedList list, Node init, Node end) {
-		
+	private Node partition(Node head, Node tail) {
+		return head;
 	}
 	
 	public long[] sort(LinkedList list) {
 		long[] swapComp = {0, 0};
 		
 		return swapComp;
+	}
+	
+	private void sort(Node head, Node tail) {
+		Node pivot = partition(head, tail);
+		Node pivotNext = pivot.getNext();
+		
+		if(head.getNext() != pivot)
+			sort(head, pivot);
+		if(pivotNext.getNext() != tail)
+			sort(pivotNext, pivotNext);
 	}
 
 }
