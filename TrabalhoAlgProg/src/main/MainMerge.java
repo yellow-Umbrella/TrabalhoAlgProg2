6d1560ed63dce7bb6c0f6cfcc7468ed9ad3f78e1
yellow-Umbrella <renanbenattidias@gmail.com>
2016-08-28 20:18:30 -0400

    ...

--- TrabalhoAlgProg/src/main/MainMerge.java
+++ TrabalhoAlgProg/src/main/MainMerge.java
@@ -21,7 +21,7 @@ public class MainMerge {
 		time2 = System.nanoTime();
 		
 		System.out.println("  1.000 - Tempo: " + (time2 - time1) + " Trocas: " + swapComp[0] + " Comparacoes: " + swapComp[1]);
-		System.out.println(list0);
+		//System.out.println(list0);
 
 		////////////////////////////////////////////////////////////
 		LinkedList list1 = file.reader("5000.txt");
@@ -31,7 +31,7 @@ public class MainMerge {
 		time2 = System.nanoTime();
 
 		System.out.println("  5.000 - Tempo: " + (time2 - time1) + " Trocas: " + swapComp[0] + " Comparacoes: " + swapComp[1]);
-		System.out.println(list1);
+		//System.out.println(list1);
 
 		////////////////////////////////////////////////////////////
 		LinkedList list2 = file.reader("10000.txt");
@@ -41,7 +41,7 @@ public class MainMerge {
 		time2 = System.nanoTime();
 
 		System.out.println(" 10.000 - Tempo: " + (time2 - time1) + " Trocas: " + swapComp[0] + " Comparacoes: " + swapComp[1]);
-		System.out.println(list2);
+		//System.out.println(list2);
 
 		////////////////////////////////////////////////////////////
 		LinkedList list3 = file.reader("20000.txt");
@@ -51,7 +51,7 @@ public class MainMerge {
 		time2 = System.nanoTime();
 
 		System.out.println(" 20.000 - Tempo: " + (time2 - time1) + " Trocas: " + swapComp[0] + " Comparacoes: " + swapComp[1]);
-		System.out.println(list3);
+		//System.out.println(list3);
 
 		////////////////////////////////////////////////////////////
 		LinkedList list4 = file.reader("50000.txt");
@@ -61,7 +61,7 @@ public class MainMerge {
 		time2 = System.nanoTime();
 
 		System.out.println(" 50.000 - Tempo: " + (time2 - time1) + " Trocas: " + swapComp[0] + " Comparacoes: " + swapComp[1]);
-		System.out.println(list4);
+		//System.out.println(list4);
 
 		////////////////////////////////////////////////////////////
 		LinkedList list5 = file.reader("100000.txt");
@@ -71,7 +71,7 @@ public class MainMerge {
 		time2 = System.nanoTime();
 
 		System.out.println("100.000 - Tempo: " + (time2 - time1) + " Trocas: " + swapComp[0] + " Comparacoes: " + swapComp[1]);
-		System.out.println(list5);
+		//System.out.println(list5);
 		
 	}
 
